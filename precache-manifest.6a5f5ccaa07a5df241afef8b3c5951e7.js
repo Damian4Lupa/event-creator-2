9749b40a2c4a2b36275d6b74d3a74c64b358754a
Damian Lupa <damian.lupa88@gmail.com>
2019-11-25 19:59:52 +0100

    Updates

---
+++ precache-manifest.6a5f5ccaa07a5df241afef8b3c5951e7.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8cfee2c5afd654ce8dbec1564bf1a81",
+    "url": "/event-creator2/index.html"
+  },
+  {
+    "revision": "0646e156ae681a570fc0",
+    "url": "/event-creator2/static/css/2.23c02e1a.chunk.css"
+  },
+  {
+    "revision": "606a1146cf59fec04d86",
+    "url": "/event-creator2/static/css/main.12b624bf.chunk.css"
+  },
+  {
+    "revision": "0646e156ae681a570fc0",
+    "url": "/event-creator2/static/js/2.c5ea9872.chunk.js"
+  },
+  {
+    "revision": "606a1146cf59fec04d86",
+    "url": "/event-creator2/static/js/main.ba6e0ab7.chunk.js"
+  },
+  {
+    "revision": "a26eb52904b6bb7289c3",
+    "url": "/event-creator2/static/js/runtime-main.246bccd7.js"
+  },
+  {
+    "revision": "35613b20f43ab402d278f02794ff8307",
+    "url": "/event-creator2/static/media/img1.35613b20.jpg"
+  },
+  {
+    "revision": "3d5cda2aab390ef1ed43bb726e06292d",
+    "url": "/event-creator2/static/media/img10.3d5cda2a.jpg"
+  },
+  {
+    "revision": "c36d06dcd2f002c97c751b577a0f0f13",
+    "url": "/event-creator2/static/media/img2.c36d06dc.jpg"
+  },
+  {
+    "revision": "482f1a1d2fdb6b591592448a907dec67",
+    "url": "/event-creator2/static/media/img3.482f1a1d.jpg"
+  },
+  {
+    "revision": "6720b611f2bd9c62badec57583f46358",
+    "url": "/event-creator2/static/media/img4.6720b611.jpg"
+  },
+  {
+    "revision": "e8873ff2d61677a02dbd8778c6b7c03b",
+    "url": "/event-creator2/static/media/img5.e8873ff2.jpg"
+  },
+  {
+    "revision": "59917bfe4fe698b437a4eee0e8276254",
+    "url": "/event-creator2/static/media/img6.59917bfe.jpg"
+  },
+  {
+    "revision": "11219a2b75214c4097f665f86be70687",
+    "url": "/event-creator2/static/media/img7.11219a2b.jpg"
+  },
+  {
+    "revision": "d1e71654b87f9bd93bb415ba1ffa8931",
+    "url": "/event-creator2/static/media/img8.d1e71654.jpg"
+  },
+  {
+    "revision": "30c6e2e87fbfb714519110c8b56b9a2f",
+    "url": "/event-creator2/static/media/img9.30c6e2e8.jpg"
+  }
+]);
