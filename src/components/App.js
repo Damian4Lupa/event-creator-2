e1f9fdceb3799d55a7f06074303a5c7776ad8f8e
Damian Lupa <49316931+Damian4Lupa@users.noreply.github.com>
2019-12-07 16:14:23 +0100

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, HashRouter, Route, Switch } from 'react-router-dom';
 import 'normalize.css'
 import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/App.css'
 import '../styles/Img.css'
-
-import Page from './Page'
+import CreateEvent from '../pages/CreateEvent'
+import Clock from '../pages/Clock'
+import ErrorPage from '../pages/ErrorPage';
 
 class App extends Component {
   state = {}
   render() {
     return (
       <Router>
-        <div>
-          <Page />
-        </div>
+        <HashRouter basename={"/"}>
+          <Switch>
+            <Route exact path="/" component={CreateEvent} />
+            <Route path="/clock" component={Clock} />
+            <Route component={ErrorPage} />
+          </Switch>
+        </HashRouter>
       </Router>
     );
   }
